f5be18b282c47716b503a738e95be1582c6759dc
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-02-26 18:28:23 +0300

    uuid package installed, jwt for login implemented

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,5 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { v4 as uuid } from 'uuid';
+
+import CustomAPIError from '../errors/custom-error.js';
+
 export const login = async (req, res) => {
-  res.send('Login');
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide username and password', 400);
+  }
+
+  const id = uuid();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+
+  console.log(token);
+
+  res.status(200).json({ msg: 'Login successful', token });
 };
 
 export const dashboard = async (req, res) => {
